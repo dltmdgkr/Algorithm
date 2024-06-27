b86f69b8a6e7ffda7d58955dd394788d4a7ade3d
dltmdgkr <126390568+dltmdgkr@users.noreply.github.com>
2024-06-27 20:02:41 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 136 ms, Memory: 9520 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+
+for (let i = 1; i <= +input; i++) {
+  console.log(" ".repeat(+input - i) + "*".repeat(i));
+}
